8868d4354acc1213baa92276a8031d8155252b36
h74.amirs <miti145@gmail.com>
2023-08-21 17:01:01 +0300

    blocking map: extended a bit the first test (pass after a long time)

--- tests/blocking_map/utest.cpp
+++ tests/blocking_map/utest.cpp
@@ -13,16 +13,32 @@ BEGIN_TEST(basic_test)
     std::vector<std::thread> threads;
     int big_number = 10'000;
     threads.reserve(big_number);
-    auto thread = [&map] {
+    auto thread_inc = [&map] {
         ++map.at("first");
     }; 
     for (int i = 0; i < big_number; ++i) {
-        threads.emplace_back(std::thread{thread});
+        threads.emplace_back(std::thread{thread_inc});
     }
     for (int i = 0; i < big_number; ++i) {
         threads.at(i).join();
-    }    
+    }
+    threads.clear();    
     ASSERT_EQUAL(map.at("first"),big_number);
+
+    map["second"] = 0;
+    auto thread_add = [&map](int const& a_times) {
+        for (int i = 0; i < a_times; ++i) {
+            ++map.at("second");
+        }
+    };
+
+    for (int i = 0; i < big_number; ++i) {
+        threads.emplace_back(std::thread(thread_add,big_number));
+    }
+    for (int i = 0; i < big_number; ++i) {
+        threads.at(i).join();
+    }
+    ASSERT_EQUAL(map.at("second"),big_number*big_number);    
 
 END_TEST
 
