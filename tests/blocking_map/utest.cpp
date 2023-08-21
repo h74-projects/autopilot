867ee462b08a432beb72add089680e2eb9c8f11c
h74.amirs <miti145@gmail.com>
2023-08-21 16:48:05 +0300

    blocking map utest: fixed first test bug

--- tests/blocking_map/utest.cpp
+++ tests/blocking_map/utest.cpp
@@ -1,6 +1,7 @@
 #include "mu_test.h"
 #include "blocking_map.hpp"
 
+#include <iostream>
 #include <string>
 #include <vector>
 #include <thread>
@@ -10,17 +11,17 @@ BEGIN_TEST(basic_test)
     concurrency::BlockingMap<std::string,int> map;
     map["first"] = 0;
     std::vector<std::thread> threads;
-    int big_number = 1'000'000;
+    int big_number = 10'000;
     threads.reserve(big_number);
     auto thread = [&map] {
         ++map.at("first");
-    };
+    }; 
     for (int i = 0; i < big_number; ++i) {
-        threads.push_back(std::thread{thread});
+        threads.emplace_back(std::thread{thread});
     }
-    for (auto& it : threads) {
-        it.join();
-    }
+    for (int i = 0; i < big_number; ++i) {
+        threads.at(i).join();
+    }    
     ASSERT_EQUAL(map.at("first"),big_number);
 
 END_TEST
