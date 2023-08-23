67714d0ba8beafc00c6196d329151f913e7607f5
Pato <miti145@gmail.com>
2023-08-23 22:58:42 +0300

    client mediator utest: added second test to test client in itself

--- tests/telnet_mediator/utest.cpp
+++ tests/telnet_mediator/utest.cpp
@@ -20,8 +20,38 @@ BEGIN_TEST(basic_test)
 
 END_TEST
 
+BEGIN_TEST(client_test)
+    TelnetClient client{"127.0.0.1", 5401, 1000};
+    try {
+        client.send("get /sim/aircraft");
+        std::string sim = client.read();
+        std::cout << '\n' << sim << '\n';
+    } catch (...) {
+        std::cout << "\nNothing to read\n";
+    }
+    client.send("set /controls/flight/aileron -1.0");
+    sleep(1);
+    client.send("set /controls/flight/aileron 100.0");
+    sleep(1);
+    client.send("set /controls/flight/aileron 1.0");
+    sleep(1);
+    client.send("set /controls/flight/aileron 45.0");
+    sleep(1);
+    client.send("set /controls/flight/aileron -1.0");
+    sleep(1);
+    client.send("set /controls/flight/aileron -100.0");
+    sleep(1);
+    client.send("set /controls/flight/aileron 1.0");
+    sleep(1);
+    client.send("get /sim/aircraft");
+
+    ASSERT_PASS();
+
+END_TEST
+
 
 BEGIN_SUITE("tel it to the judge")
     TEST(basic_test)
+    TEST(client_test)
 
 END_SUITE
