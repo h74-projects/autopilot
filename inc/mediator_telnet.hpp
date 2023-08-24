51020cf9586843b05de7b6823d85dd1b8048da2a
Pato <miti145@gmail.com>
2023-08-24 19:59:08 +0300

    telnet mediator: added dtor and get updated func

--- inc/mediator_telnet.hpp
+++ inc/mediator_telnet.hpp
@@ -24,8 +24,8 @@ namespace fgear {
 
 class TelnetMediator : public Mediator {
 public:
-    TelnetMediator(std::string const & a_server_ip, std::string const & a_telnet_ip, uint32_t const& a_telnet_port, uint32_t const& a_udp_port);
-    ~TelnetMediator() = default;
+    TelnetMediator(std::string const & a_server_ip, std::string const & a_telnet_ip, uint32_t const& a_telnet_port, int32_t const& a_udp_port);
+    ~TelnetMediator();
 
     void set(std::string const& a_key ,Var const& a_var) override;
 
@@ -34,6 +34,8 @@ public:
 private:
     std::string make_command(std::string const& a_key ,Var const& a_var, std::string const& a_command);
     void fill_map(std::string const& a_filename, concurrency::BlockingMap<std::string, std::tuple<std::string, std::string, Var>>& a_variables);
+    void get_updates();
+    void update_map(std::string const& a_message, ssize_t a_len);
     
 private:
     //TODO: instead of string key we need some variable type enum
@@ -41,9 +43,10 @@ private:
     concurrency::BlockingMap<std::string, std::tuple<std::string, std::string, Var>> m_variables;
     //TODO: in the future abstract Client class
     std::mutex m_mtx;    
-    boost::asio::io_context m_context;;
-    communication::UDPServer m_listen;
+    boost::asio::io_context m_context;
+    communication::UDPServer m_server;
     TelnetClient m_telnet;
+    bool m_active;
 };
 
 } // namespace fgear
--- src/mediator_telnet.cpp
+++ src/mediator_telnet.cpp
@@ -9,11 +9,18 @@ constexpr uint32_t TIME_OUT = 500;
 namespace fgear {
 
 //TODO: load map from file
-TelnetMediator::TelnetMediator(std::string const & a_server_ip, std::string const & a_telnet_ip, uint32_t const& a_telnet_port, uint32_t const& a_udp_port)
-: m_listen{m_context, a_server_ip, a_udp_port}
+TelnetMediator::TelnetMediator(std::string const & a_server_ip, std::string const & a_telnet_ip, uint32_t const& a_telnet_port, int32_t const& a_udp_port)
+: m_server{m_context, a_server_ip, a_udp_port}
 , m_telnet{a_telnet_ip, a_telnet_port,TIME_OUT}
+, m_active{true}
 {
     fill_map("../../xml files/generic_small.xml", m_variables);
+}
+
+TelnetMediator::~TelnetMediator()
+{
+    m_active = false;
+    m_server.stop_listening();
 }
 
 void TelnetMediator::set(std::string const& a_key ,Var const& a_var)
@@ -64,6 +71,19 @@ void TelnetMediator::fill_map(std::string const& a_filename, concurrency::Blocki
             a_variables[name] = std::make_tuple(node_path, type, Var(format));
         }
     }
+}
+
+void TelnetMediator::get_updates()
+{
+    while (m_active) {
+        m_server.start_listening([&, this](const std::string& data, ssize_t size) {
+            update_map(data, size);
+        });
+    }
+}
+
+void TelnetMediator::update_map(std::string const& a_message, ssize_t a_len)
+{
 }
 
 } // namespace fgear
