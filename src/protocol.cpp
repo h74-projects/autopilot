f4b02626ce42544030a08918f3f8fd21b41422af
Pato <miti145@gmail.com>
2023-08-31 20:52:39 +0300

    protocol: changed while loop to for in unpack

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -12,7 +12,8 @@ void Protocol::unpack(std::string const& a_package)
     size_t name_index{};
     size_t value_index = a_package.find(":") + 1;
     size_t end_index = a_package.find(",");
-    while (end_index != std::string::npos) {
+    size_t size = m_variables.get()->size();
+    for (size_t i = 0; i < size; ++i) {
         std::string name = a_package.substr(name_index, value_index - 1 - name_index);
         float value = std::stof(a_package.substr(value_index, end_index - value_index));
         m_variables.get()->exchange_if(name, value);
