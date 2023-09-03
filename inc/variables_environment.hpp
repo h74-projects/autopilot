d120b3ccc7ea2be3a299c79456e86fd94ca0a5b9
Pato <miti145@gmail.com>
2023-09-03 14:03:02 +0300

    new environment class

---
+++ inc/variables_environment.hpp
@@ -0,0 +1,36 @@
+#ifndef VARIABLES_ENVIRONMENT_HPP
+#define VARIABLES_ENVIRONMENT_HPP
+
+#include "variables.hpp"
+#include "telemetry.hpp"
+#include "middle_man.hpp"
+
+#include <memory> // shared and unique ptr
+#include <string> // std::string
+
+namespace fgear {
+
+class Environment {
+public:
+    explicit Environment(std::shared_ptr<Variables> a_binded, std::unique_ptr<MiddleMan>&& a_middle_man);
+    ~Environment() = default;
+
+    Environment(Environment const&) = delete;
+    Environment(Environment&&) = delete;
+
+    Environment& operator=(Environment const&) = delete;
+    Environment& operator=(Environment&&) = delete;
+
+    void set(std::string const& a_key, float const& a_value);
+
+    float get(std::string const& a_key) const;
+
+private:
+    std::unique_ptr<MiddleMan> m_middle_man;
+    std::shared_ptr<Variables> m_bind;
+    Variables m_local;
+};
+
+} // namespace fgear
+
+#endif // VARIABLES_ENVIRONMENT_HPP
