2615c7bf0f40142d95ac6bb72097aa9f47e9bd95
Pato <miti145@gmail.com>
2023-09-04 19:24:16 +0300

    created ast while node hpp and cpp files

---
+++ inc/ast_node_while.hpp
@@ -0,0 +1,25 @@
+#ifndef AST_NODE_WHILE_HPP
+#define AST_NODE_WHILE_HPP
+
+#include "ast_node.hpp"
+#include "ast_node_condition.hpp"
+
+#include <vector> // vector
+#include <memory> // unique ptr
+
+namespace fgear {
+
+class WhileNode : public ASTNode {
+public:
+    WhileNode(std::unique_ptr<ConditionNode> a_cond, std::vector<std::unique_ptr<ASTNode>> a_body);
+
+    void operator()() override;
+
+private:
+    std::unique_ptr<ConditionNode> m_condition;
+    std::vector<std::unique_ptr<ASTNode>> m_loop_body;
+};
+
+} // namespace fgear
+
+#endif 
---
+++ src/ast_node_while.cpp
@@ -0,0 +1,20 @@
+#include "ast_node_while.hpp"
+
+namespace fgear {
+
+WhileNode::WhileNode(std::unique_ptr<ConditionNode> a_cond, std::vector<std::unique_ptr<ASTNode>> a_body)
+: m_condition{std::move(a_cond)}
+, m_loop_body{std::move(a_body)}
+{
+}
+
+void WhileNode::operator()()
+{
+    while (m_condition.get()->condition()) {
+        for (auto& it : m_loop_body) {
+            it.get()->operator()();
+        }
+    }
+}
+
+} // namespace fgear
