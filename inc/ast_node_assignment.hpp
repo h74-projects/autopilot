7ef3b7185b4a7df0ac8a526d6bd9a0c52858ecfb
Pato <miti145@gmail.com>
2023-09-06 18:22:32 +0300

    new assignment node class

---
+++ inc/ast_node_assignment.hpp
@@ -0,0 +1,25 @@
+#ifndef AST_NODE_ASSIGNMENT_HPP
+#define AST_NODE_ASSIGNMENT_HPP
+
+#include "ast_node.hpp"
+
+#include <string> //std string
+#include <memory> // unique ptr
+
+namespace fgear {
+
+//TODO: will add to non bind map
+class AssignmentNode : public ASTNode {
+public:
+    AssignmentNode(const std::string& a_identifier, float const& a_value);
+
+    void visit(Interpreter const& a_interpreter) override;
+
+private:
+    std::string m_identifier;
+    float m_value;
+};
+
+} // namespace fgear
+
+#endif // AST_NODE_ASSIGNMENT_HPP
