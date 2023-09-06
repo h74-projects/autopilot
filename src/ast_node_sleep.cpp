3dfd7cf80deb2ee5be787011783f19bbb4c4349a
Pato <miti145@gmail.com>
2023-09-06 18:49:05 +0300

    created sleep node hpp and cpp

---
+++ src/ast_node_sleep.cpp
@@ -0,0 +1,21 @@
+#include "ast_node_sleep.hpp"
+
+namespace fgear {
+
+SleepNode::SleepNode(std::string const& a_time)
+: m_time(a_time) 
+{ 
+}
+
+
+int SleepNode::time() const
+{
+    return std::stoi(m_time);
+}
+
+void SleepNode::visit(Interpreter const& a_interpreter)
+{
+    ::usleep(std::stoi(m_time));
+}
+
+} // namespace fgear
