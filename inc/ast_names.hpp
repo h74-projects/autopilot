1781917239c8bdfec393857d525683f6e839cb91
Pato <miti145@gmail.com>
2023-09-06 18:37:18 +0300

    created hpp ast_names for all node class names

---
+++ inc/ast_names.hpp
@@ -0,0 +1,26 @@
+#ifndef AST_NAMES_HPP
+#define AST_NAMES_HPP
+
+namespace fgear {
+
+class ArithmeticNode;
+
+class AssignmentNode;
+
+class BindNode;
+
+class ConditionNode;
+
+class IfNode;
+
+class PrintNode;
+
+class VariableNode;
+
+class SleepNode;
+
+class WhileNode;
+
+}
+
+#endif // AST_NAMES_HPP
